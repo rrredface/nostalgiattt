6c9582597020ce8a6ce8d79dfce2bfaeaebe9fe0
rrredface <5395733+rrredface@users.noreply.github.com>
2023-06-27 11:00:53 -0500

    fix disguiser but for real this time

--- lua/autorun/server/pointshop_disguiser.lua
+++ lua/autorun/server/pointshop_disguiser.lua
@@ -1,50 +1,35 @@
-local cosmetics = {}
+local disguisemodels = { -- Add playermodels to disguise as here.
+"models/player/arctic.mdl",
+"models/player/guerilla.mdl",
+"models/player/leet.mdl",
+"models/player/phoenix.mdl"
+}
 
-local function GetPlayerCosmetics(ply)
-	local items = {}
-	for id, _ in pairs(ply.PS_Items) do
- 		if ply:PS_HasItemEquipped(id) then
- 			table.insert(items, id)
- 		end
-	end
-	return items
-end
-
-local function HolsterCosmetics(ply)
-	local items = GetPlayerCosmetics(ply)
-	if not cosmetics[ply:AccountID()] then
-		table.insert(cosmetics, ply:AccountID(), items)
-	end
-	for _, item  in pairs(items) do
-		ply:PS_HolsterItem(item)
-	end
-end
-
-local function RestoreCosmetics(ply)
-	local items = cosmetics[ply:AccountID()]
-	if items then
-		for _, item in pairs(items) do
-			ply:PS_EquipItem(item)
-		end
-	end
-	cosmetics[ply:AccountID()] = nil
-end
+local models = {} -- for remembering players' equipped models
 
 hook.Add("TTTToggleDisguiser", "TTTDisguiserCosmetics", function(ply, disg)
 	if IsValid(ply) then
 		if disg then
-			HolsterCosmetics(ply)
+			table.insert(models, ply:UserID(), ply:GetModel())
+			math.randomseed(os.time())
+			ply:SetModel(disguisemodels[math.random(#disguisemodels)])
+			for id, item in pairs(ply.PS_Items) do
+				if item.Equipped then
+					local ITEM = PS.Items[id]
+					ITEM:OnHolster(ply, item.Modifiers)
+				end
+			end
 		else
- 			RestoreCosmetics(ply)
+			local mdl = models[ply:UserID()]
+			if mdl then
+				ply:SetModel(mdl)
+			end
+			for id, item in pairs(ply.PS_Items) do
+				if item.Equipped then
+					local ITEM = PS.Items[id]
+					ITEM:OnEquip(ply, item.Modifiers)
+				end
+			end
 		end
 	end
 end)
-
--- restore a player's cosmetics on respawn if they die while disguised
-hook.Add("PlayerSpawn", "RestoreCosmeticsOnDeath", function(ply, _, _)
-	RestoreCosmetics(ply)
-end)
-
--- some cases in which people's cosmetics will have to be manually re-equipped:
--- disconnect while disguised, server restart while disguised.
--- for now i don't think it's worth worrying about.
